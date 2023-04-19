7f773a3f48cdd0ad10425b8252636b36b69d1a11
araujo <araujoyadir@gmail.com>
2023-04-19 21:15:41 +0200

    hasta las 21:00

--- src/com/hedima/modelo/Figura.java
+++ src/com/hedima/modelo/Figura.java
@@ -1,2 +1,37 @@
-package com.hedima.modelo;public class Figura {
+package com.hedima.modelo;
+
+public abstract class Figura {
+    private int x;
+    private int y;
+
+    public abstract double calcularArea();
+
+
+    public String mostrarPosicion(){
+        return "(" + x + "," + y + ")";
+    }
+
+    public Figura() {
+    }
+
+    public Figura(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
--- src/com/hedima/presentacion/ProbarPolimorfismoFigura.java
+++ src/com/hedima/presentacion/ProbarPolimorfismoFigura.java
@@ -1,2 +1,32 @@
-package com.hedima.presentacion;public class ProbarPolimorfismoFigura {
+package com.hedima.presentacion;
+
+import com.hedima.modelo.Cuadrado;
+import com.hedima.modelo.Figura;
+import com.hedima.modelo.Triangulo;
+
+public class ProbarPolimorfismoFigura {
+    public static void main(String[] args) {
+        Figura f1 = new Triangulo(4,4,3,5);
+        System.out.println("Desde f1 solo se ven los atributos de Figura x: " + f1.getX() + "y" + f1.getY());
+        Triangulo t1 = (Triangulo) f1;
+        System.out.println("Desde t1 solo se ven los atributos de Triangulo x: " + t1.getX() + "y" + t1.getY() + "base" + t1.getBase() + "altura" + t1.getAltura());
+        System.out.println(f1.mostrarPosicion());
+        System.out.println(t1.mostrarPosicion());
+        Figura f2 = new Cuadrado(0,0,5);
+        System.out.println("Desde f2 sólo se miran los atributos de figura x "+ f2.getY() + " y " + f2.getY());
+        Cuadrado c1 = (Cuadrado)  f2;
+        System.out.println("Se puede visualizar todos lo de Cuadrado lado " + c1.getLado());
+        System.out.println(f2.mostrarPosicion());
+        System.out.println(c1.mostrarPosicion());
+
+        Figura[] figuras = new Figura [2];
+        figuras[0] = f1;
+        figuras[1] = f2;
+        System.out.println("Bucle");
+        for(int i=0; i<figuras.length; i++){
+            System.out.println(figuras[i].mostrarPosicion());
+            System.out.println(figuras[i].calcularArea());
+        }
+
+    }
 }
